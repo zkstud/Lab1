8d52d2482e0f1ce1af6285c53fb4838e57c52f08
zkstud <100755286+zkstud@users.noreply.github.com>
2022-03-05 13:24:50 +0100

    tabelkę logarytmów o podstawie 2 oraz akumulowaną sumę kolejnych potęg liczby 2

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,17 +2,10 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 3;
-        double b = 4.21;
-        String s = "jakis tekst";
-        String ss = "Nazywaliśmy to \"Witamina B3\"";
-
-        System.out.printf("a = %d    b = %.2f    s = %s20s %n", a, b, s);
-        System.out.println(ss);
-
-        System.out.printf("alfa\t\tsin(alfa)\n");
-        for (int i = 0; i < 370; i+=10) {
-            System.out.printf("%d\t\t\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
+        int ii = 0;
+        System.out.printf("arg\t\tlog2(arg)\tsum(arg)");
+        for (int i = 0; i < 20; i++) {
+            System.out.printf("%d\t\t%.2f\t\t%d\t\n", i, Math.log(i) / Math.log(2), ii+=i);
         }
     }
 }
