c6eb2eb7ef483cf1e3302098d4bf56de20f6e87c
zkstud <100755286+zkstud@users.noreply.github.com>
2022-03-05 12:58:20 +0100

    dodalem tabelke funkci sin dla kontow od 0 do 360 stopni co 10 stopni

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,11 +2,17 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Ala");
-        System.out.print("ma");
-        System.out.print("kota");
-        System.out.print("Ala\n");
-        System.out.print("ma\n");
-        System.out.print("kota\n");
+        int a = 3;
+        double b = 4.21;
+        String s = "jakis tekst";
+        String ss = "Nazywaliśmy to \"Witamina B3\"";
+
+        System.out.printf("a = %d    b = %.2f    s = %s20s %n", a, b, s);
+        System.out.println(ss);
+
+        System.out.printf("alfa\t\tsin(alfa)\n");
+        for (int i = 0; i < 370; i+=10) {
+            System.out.printf("%d\t\t\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
+        }
     }
 }
