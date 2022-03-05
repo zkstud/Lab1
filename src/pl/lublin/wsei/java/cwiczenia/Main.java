f66c4c611cbf77341962cd57b2d17ec66a005d29
zkstud <100755286+zkstud@users.noreply.github.com>
2022-03-05 12:46:41 +0100

    dodalem napis "Ala ma kota"

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,6 +2,11 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello java world");
+        System.out.print("Ala");
+        System.out.print("ma");
+        System.out.print("kota");
+        System.out.print("Ala\n");
+        System.out.print("ma\n");
+        System.out.print("kota\n");
     }
 }
