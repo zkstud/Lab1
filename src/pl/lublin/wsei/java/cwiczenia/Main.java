7685953dd1e5cff729b10810d3b8d2315ca5db08
zkstud <100755286+zkstud@users.noreply.github.com>
2022-03-05 13:39:13 +0100

    dodalem 30 losowych liczb całkowitych i obliczylem ich minimum, maksimum i średnią

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,31 +1,24 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("\t***Program do wypisania postaci dwojkowej i szesnastkowej***\t");
-        int num;
-        String bin, hex = "";
-        do {
-            System.out.print("\nPodaj liczbe: ");
-            num = in.nextInt();
-            if (num == 0) break;
+        int[] liczby = new int[30];
+        Random rnd = new Random();
 
-            bin = Integer.toBinaryString(num);
-            hex = Integer.toHexString(num);
+        for (int i = 0; i < 30; i++) liczby[i] = rnd.nextInt();
 
-            System.out.printf("DEC = %d\t BIN = %s\t HEX = %s", num, bin, hex);
-            System.out.printf("\nDEC = %d\t BIN = %s\t HEX = %s", num,
-                    leftPad(bin, '0', 4), leftPad(hex.toUpperCase(), '0', 4));
-        } while (true);
-    }
-    public static String leftPad(String aText, char aChar, int aWidth) { //bez static blad kompilacji;
-        // private - nic sie nie stanie, ale metoda
-        // bedzie dostepna tylko w danej klasie
-        String res = aText;
-        for (int i = 0; i < aWidth; i++) res = aChar + res;
-        return res;
+        int mx = Integer.MIN_VALUE;
+        int mn = Integer.MAX_VALUE;
+        long avg = 0;
+        for (int l : liczby) {
+            System.out.println(l);
+            if (l < mn) mn = 1;
+            if (l > mx) mx = 1;
+            avg += 1;
+        }
+        System.out.printf("MIN = %d, MAX = %d, AVG = %f", mn, mx, (float) avg / liczby.length);
     }
 }
