7e73ab39ee28be66556a890e9caf056c4de6dfac
zkstud <100755286+zkstud@users.noreply.github.com>
2022-03-05 13:34:59 +0100

    napisalem program do wypisania postaci dwójkowej i szesnastkowej podanej przez użytkownika liczby

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -4,14 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int num1, num2 = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("\t***Program do wypisania postaci dwojkowej i szesnastkowej***\t");
+        int num;
+        String bin, hex = "";
         do {
-            System.out.print("Podaj liczbe ktora mam dodac: ");
-            num1 = input.nextInt();
-            num2 = input.nextInt();
-            if ((num1 == 0) || (num2 == 0)) break;
-            System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
-        }while(true);
+            System.out.print("\nPodaj liczbe: ");
+            num = in.nextInt();
+            if (num == 0) break;
+
+            bin = Integer.toBinaryString(num);
+            hex = Integer.toHexString(num);
+
+            System.out.printf("DEC = %d\t BIN = %s\t HEX = %s", num, bin, hex);
+            System.out.printf("\nDEC = %d\t BIN = %s\t HEX = %s", num,
+                    leftPad(bin, '0', 4), leftPad(hex.toUpperCase(), '0', 4));
+        } while (true);
+    }
+    public static String leftPad(String aText, char aChar, int aWidth) { //bez static blad kompilacji;
+        // private - nic sie nie stanie, ale metoda
+        // bedzie dostepna tylko w danej klasie
+        String res = aText;
+        for (int i = 0; i < aWidth; i++) res = aChar + res;
+        return res;
     }
 }
